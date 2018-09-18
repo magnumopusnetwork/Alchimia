6253fee74dc6fbe939e3bb003e42fabef227ccfe
Stephen-MagnumOpus <stephen@magnumopusnetwork.com>
2018-09-18 16:37:13 -0400

    Updaed units.

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,7 @@ ALCHIMIAQT=${ALCHIMIAQT:-$SRCDIR/qt/alchimia-qt}
 [ ! -x $ALCHIMIAD ] && echo "$ALCHIMIAD not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($ALCHIMIACLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+ALKVER=($($ALCHIMIACLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
@@ -22,8 +22,8 @@ $ALCHIMIAD --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $ALCHIMIAD $ALCHIMIACLI $ALCHIMIATX $ALCHIMIAQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${ALKVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${ALKVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case BTC: return QString("ALK");
+    case mBTC: return QString("AGs");
+    case uBTC: return QString("PBs");
     default: return QString("???");
     }
 }
@@ -52,8 +52,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Alchimias");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("AGs (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("PBs (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
