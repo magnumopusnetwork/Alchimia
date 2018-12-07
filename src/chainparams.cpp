a84c9d67b5b19f321b4db42e64198b66bf4e2d8a
Stephen-MagnumOpus <stephen@magnumopusnetwork.com>
2018-12-07 17:38:29 -0500

    Updated check points.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -146,24 +146,13 @@ public:
         checkpointData = (CCheckpointData) {
             {
                 {     0, uint256S("0x424a21c65b4c5bae503ecb2a5fccc6f804758a7c46c3a02255d28ed167388626")},
-            	/*
-                {  1500, uint256S("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967")},
-                {  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846")},
-                {  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70")},
-                { 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d")},
-                { 23420, uint256S("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507")},
-                { 50000, uint256S("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6")},
-                { 80000, uint256S("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a")},
-                {120000, uint256S("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131")},
-                {161500, uint256S("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43")},
-                {179620, uint256S("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709")},
-                {240000, uint256S("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa")},
-                {383640, uint256S("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364")},
-                {409004, uint256S("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3")},
-                {456000, uint256S("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004")},
-                {638902, uint256S("0x15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38")},
-                {721000, uint256S("0x198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5")},
-                */
+                {   500, uint256S("0x1ad12e82236ff25934a9baf9976c79f365ffeed17202ed2aa16bf1d2f2308e9e")},
+                {  1000, uint256S("0x950a3e846b801085c2abb5b0df6caa4ceac749f57b04c6081d0dbf44b4380129")},
+                {  2500, uint256S("0x91afdad63b94c17984dc55be5764cf4cfd2b3ce84f2592f4cc42af73ed6fab17")},
+                {  5000, uint256S("0x3c07b88a3d6ee61986c0f58a17329f8eaeaf2d310d04e19ed2bb0450fda3bd4e")},
+                { 10000, uint256S("0x54f449029af9dd636771e22ddf1db6ddb40ed080d3b172deaa380c42ce90e086")},
+                { 25000, uint256S("0xb72afbfdc87444aae8c5c767f0af149283569e15efd6cfadbf473509854e9959")},
+                { 50000, uint256S("0x7690674c7fa79440269c74d7fdc4b15b449a701c7c0e46116604cfb19a125125")},
             }
         };
 
